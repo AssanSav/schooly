1e00fcee8a227b6675a017393bef879525df1018
AssanSav <assane.savadogo81@gmail.com>
2020-09-03 11:06:47 -0700

    Users index action controller'

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
     def login 
-      user = User.find_by_email(params[:email])
+      user = User.find_by_email(params[:email].upcase)
       if user && user.authenticate(params[:password])
         session[:user_id] = user.id
         render json: {
@@ -36,7 +36,7 @@ class SessionsController < ApplicationController
       end
     end
 
-    def logout
+    def destroy
       user = User.find(session[:user_id])
       if user && session.clear
         render json: {
