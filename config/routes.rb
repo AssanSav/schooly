7840d43c155622b42d88155cdee57a3f5686f483
AssanSav <assane.savadogo81@gmail.com>
2020-09-03 11:07:31 -0700

    Users and Sessions routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do 
+    namespace :v1 do 
+      resources :users
+    end
+  end
+
+  get "api/v1/session/status", to: "api/v1/sessions#is_logged_in?"
+  delete "api/v1/logout/:id", to: "api/v1/sessions#destroy"
+  post "api/v1/login", to: "api/v1/sessions#login"
 end
