054316bcccf93f9100706de94ad8ec1b18acbe43
AssanSav <assane.savadogo81@gmail.com>
2020-09-03 09:58:49 -0700

    Passaword_digest attributes added to the users table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(first_name: "assane", last_name: "savadogo", email: "assane.savadogo81@gmail.com", major: "computer science", department: "tech", admin: true, teacher: false, student: false, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSgMTnyu57w3n7MiSYwH99BIzbT-5HoQUA8Jw&usqp=CAU", password: "assane", password_confirmation: "assane" )
