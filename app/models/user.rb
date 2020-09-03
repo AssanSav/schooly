3f11720e75aade524685e0b5038fb093b018d358
AssanSav <assane.savadogo81@gmail.com>
2020-09-03 12:11:17 -0700

    Admin, teacher or student must be set before creating a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,16 @@ class User < ApplicationRecord
     validates_uniqueness_of :email
     validates_presence_of :first_name, :last_name, :email, :image, :major, :department 
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    before_validation :validate_user
     before_save :capitalize
 
     private
+    def validate_user
+        unless self.admin == true || self.teacher == true || self.student == true
+            errors.add(:base, "Check either admin, teacher or student")
+        end
+    end
+
     def capitalize
         capitalize_attr = ["first_name", "last_name", "image", "major", "department" ]
         self.attributes.each do |attr, val| 
