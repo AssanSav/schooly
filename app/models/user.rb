b7e56ec3c0bc22454e6f5368c726dd20f13b858f
AssanSav <assane.savadogo81@gmail.com>
2020-09-03 09:56:53 -0700

    Titleize and upcase attributes values by condition

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,21 @@
 class User < ApplicationRecord
     has_secure_password
-    has_one_attached :image
+    has_one_attached :avater
 
     validates_uniqueness_of :email
-    validates_presence_of :first_name, last_name, :email, :image, :major, :department 
+    validates_presence_of :first_name, :last_name, :email, :image, :major, :department 
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-    before_validation :capitalize_email
+    before_save :capitalize
 
-    def capitalize_email
-        self.email = self.email.capitalize
+    private
+    def capitalize
+        capitalize_attr = ["first_name", "last_name", "image", "major", "department" ]
+        self.attributes.each do |attr, val| 
+            if attr == "email"
+                self.email = self.email.upcase
+            else
+                self.send("#{attr}=", val.to_s.strip.titleize) if capitalize_attr.include?(attr)
+            end
+        end
     end
-    
 end
