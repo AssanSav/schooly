4b4dbfa0b5b4c3ec35157b189f7d79125fcef352
AssanSav <assane.savadogo81@gmail.com>
2020-09-02 20:26:45 -0700

    Validations and instance method to capitalize email before creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,13 @@ class User < ApplicationRecord
     has_secure_password
     has_one_attached :image
 
+    validates_uniqueness_of :email
     validates_presence_of :first_name, last_name, :email, :image, :major, :department 
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    before_validation :capitalize_email
+
+    def capitalize_email
+        self.email = self.email.capitalize
+    end
+    
 end
