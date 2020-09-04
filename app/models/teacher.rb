d68c994003bdbf383e7f6a29ae2e967625c07e99
AssanSav <assane.savadogo81@gmail.com>
2020-09-04 12:52:45 -0400

    Teachers relationships, attributes validations and custom methods added

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,2 +1,28 @@
 class Teacher < ApplicationRecord
+    has_secure_password
+    has_one_attached :avater
+
+    validates_uniqueness_of :email
+    validates_presence_of :first_name, :last_name, :email, :image, :department 
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    before_validation :validate_user
+    before_save :capitalize
+
+    private
+    def validate_user
+        unless self.admin == true || self.teacher == true 
+            errors.add(:base, "Check either admin, teacher or student")
+        end
+    end
+
+    def capitalize
+        capitalize_attr = ["first_name", "last_name", "major", "department" ]
+        self.attributes.each do |attr, val| 
+            if attr == "email"
+                self.email = self.email.upcase
+            else
+                self.send("#{attr}=", val.to_s.strip.titleize) if capitalize_attr.include?(attr)
+            end
+        end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(first_name: "assane", last_name: "savadogo", email: "assane.savadogo81@gmail.com", major: "computer science", department: "tech", admin: false, teacher: false, student: false, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSgMTnyu57w3n7MiSYwH99BIzbT-5HoQUA8Jw&usqp=CAU", password: "assane", password_confirmation: "assane", remember_me: false)
+Student.create(first_name: "assane", last_name: "savadogo", email: "assane.savadogo81@gmail.com", department: "tech", admin: false,  image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSgMTnyu57w3n7MiSYwH99BIzbT-5HoQUA8Jw&usqp=CAU", password: "assane", password_confirmation: "assane")
