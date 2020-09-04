94cdd2d9bfbc694a6c0260726e6182174a09abcb
AssanSav <assane.savadogo81@gmail.com>
2020-09-04 14:47:38 -0400

    Teachers and Students data serialized

--- app/serializers/student_serializer.rb
+++ app/serializers/student_serializer.rb
@@ -1,4 +1,4 @@
 class StudentSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :first_name, :last_name, :email, :image, :major
+  attributes :first_name, :last_name, :email, :image, :major, :deleted
 end
--- app/serializers/teacher_serializer.rb
+++ app/serializers/teacher_serializer.rb
@@ -1,4 +1,4 @@
 class TeacherSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :first_name, :last_name, :email, :image, :department, :admin
+  attributes :first_name, :last_name, :email, :image, :department, :admin, :deleted
 end
